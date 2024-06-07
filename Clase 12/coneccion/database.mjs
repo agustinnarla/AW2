cf1d3e696406210cd2b6e48aac27a5799fc4ff10
agustin <agustin@arla.com.ar>
2024-06-07 12:25:53 -0300

    sql

--- Clase 12/coneccion/database.mjs
+++ Clase 12/coneccion/database.mjs
@@ -1,5 +1,5 @@
 import pg from 'pg'
-const {Pool} = pg 
+
 
 export const pool = new pg.Pool({
     host: "localhost",
--- Clase 12/ruta/ruta.mjs
+++ Clase 12/ruta/ruta.mjs
@@ -5,8 +5,26 @@ const ruta =  Router()
 
 
 ruta.get('/productos',async(peticion,respuesta)=>{
-    const respost = await pool.query("SELECT * FROM productos")
-    respuesta.send(respost)
+    const res = await pool.query("SELECT * FROM productos")
+    respuesta.json(res.rows)
+})
+
+ruta.get('/productos/:id',async(peticion,respuesta) =>{
+    const {id} = peticion.params
+    const res = await pool.query(`SELECT * FROM productos WHERE id = ${id}`)
+    respuesta.json(res.rows)
+})
+
+ruta.post('/productos',async(peticion,respuesta) =>{
+    const {producto,precio} = peticion.body
+    const res = await pool.query("INSERT INTO productos (producto,precio)" + "VALUES ($1,$2)",[producto,precio])
+    respuesta.json(res.rows)
+})
+
+ruta.delete('/productos/:id',async(peticion,respuesta) =>{
+    const {id} = peticion.params
+    const res = await pool.query(`DELETE FROM productos WHERE id=${id}` )
+    respuesta.json(res.rows)
 })
 
 export default ruta;
