0edd7a59ba2d7e958336845a63b9efe1898fafd1
Agus <agustin@arla.com.ar>
2024-06-14 12:28:02 -0300

    login,registrar

--- Clase 13/app copy.mjs
+++ Clase 13/app copy.mjs
@@ -1,5 +1,5 @@
 import express from 'express';
-import dotev from 'dotenv'
+import 'dotenv/config'
 import cors from 'cors'
 import helmet from 'helmet';
 import jwt from 'jsonwebtoken'
@@ -7,52 +7,20 @@ import pg from 'pg'
 import cookieParser from 'cookie-parser';
 import bcryptjs from 'bcryptjs'
 
-dotev.config()
 const app = express()
 
 const PORT = process.env.PORT ?? 3000 
 const SECRETO = process.env.SECRETO  
-const DB_PASS = process.env.DB_PASS  
-const DB_NAME = process.env.DB_NAME  
-const DB_USER = process.env.DB_USER  
 
-const pool = new pg.Pool({
-    password: DB_PASS,
-    database: DB_NAME,
-    user: DB_USER,
-    host:'localhost',
-    port:5432
-})
-
-
-const verificarLogin = (req, res, next) => {
-    if (req.cookies && req.cookies.auth) {
-        const token = req.cookies.auth;
-        try {
-            jwt.verify(token, SECRETO);
-            next(); 
-        } catch (error) {
-            res.redirect('/');
-        }
-    } else {
-        res.redirect('/'); 
-    }
-};
 app.use(helmet())
 
-//app.use(express.json())
+app.use(express.json())
 app.use(express.urlencoded({extended:true}))
-app.use(cookieParser())
-const ruta = express.Router()
 
-app.use(ruta)
 
 app.use(express.static('www'))
-app.use('/admin',verificarLogin,express.static('admin'))
 
-app.post('/registro',(req,res)=>{
-    const {usuario,pass} = req.body
-})
+
 app.post('/login',cors(),(req,res) =>{
 
     //Recibir datos del usuario
@@ -69,7 +37,7 @@ app.post('/login',cors(),(req,res) =>{
             httpOnly:true,
             sameSite:'strict'
         });
-        res.redirect('/admin')
+        res.send(token)
     }else{
         res.redirect('/')
     }
